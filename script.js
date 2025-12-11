3195fd04ae0dc54afe3bfa749e64e04649dfe0a8
silverwolf1223 <101886723+silverwolf1223@users.noreply.github.com>
2025-12-10 20:29:53 -0600

    added data display and hourly data

--- script.js
+++ script.js
@@ -1,8 +1,17 @@
 const select = document.querySelector("select");
 const searchbar = document.querySelector("#search");
+const toggleSwitch = document.querySelector("#toggle");
 const locationHeader = document.querySelector("#locationHeader");
+const tableHours = document.querySelectorAll("#hours th");
+const tableData = document.querySelectorAll("#hourlyData td");
+const icon = document.querySelector("#icon");
 
-let place = "Adams, Nebraska";
+
+let toCelcius = false;
+
+let place = "Helotes, Texas";
+
+let hourlyData = "hourTemp";
 
 //display
 const datas = ["Temp", "TempMax", "TempMin", "FeelsLike", "Humidity", "WindSpeed", "RainChance", "icon", "conditions"]
@@ -11,6 +20,8 @@ async function getWeather(place){
     try {
         const weather = await fetch("https://weather.visualcrossing.com/VisualCrossingWebServices/rest/services/timeline/" + place + "?key=36E856BDYTHQXCQ3T87HLYQNZ");
         const weatherData = await weather.json();
+
+        updateHourly(weatherData.days[0]);
         updateDisplay(weatherData.days[0]);
     } catch (err) {
         console.log(err);
@@ -21,8 +32,60 @@ async function getWeather(place){
 function updateDisplay(wData){
     locationHeader.textContent = place;
     datas.forEach(data => {
-        document.querySelector(`#${data}`).textContent =  data + ": " + dataSelection(data, wData);
+        if(!document.querySelector(`#${data}`)){}
+        else if(toCelcius && (data == "Temp" || data == "TempMax" || data == "TempMin" || data == "FeelsLike"))
+        {
+            document.querySelector(`#${data}`).textContent =  data + ": " + parseFloat(((dataSelection(data, wData) - 32) * (5/9)).toFixed(1)) + "°C";
+        }
+        else if(data == "Temp" || data == "TempMax" || data == "TempMin" || data == "FeelsLike")
+        {
+            document.querySelector(`#${data}`).textContent =  data + ": " + dataSelection(data, wData) + "°F";
+        }
+        else if(data == "RainChance" || data == "Humidity"){
+            document.querySelector(`#${data}`).textContent =  data + ": " + dataSelection(data, wData) + "%";
+        }
+        else{
+            document.querySelector(`#${data}`).textContent =  data + ": " + dataSelection(data, wData);
+        }
     })
+
+    icon.src = `./imgs/${wData.icon}.svg`
+    
+    hourlyDisplay(hourlyData);
+}
+
+function hourlyDisplay(displayType){
+
+    times.forEach((time, index) => {
+        if(time > 12){
+            tableHours[index].textContent = time - 12 + "PM";
+        }
+        else{
+            tableHours[index].textContent = time + "AM";
+        }
+    })
+
+    switch(displayType){
+        case "hourTemp":
+            hourlyTemps.forEach((temp, index) => {
+                if(!toCelcius){
+                    tableData[index].textContent = temp + "°F";
+                } else {
+                    tableData[index].textContent = parseFloat(((temp - 32) * (5/9)).toFixed(1)) + "°C";
+                }
+            })
+            break;
+        case "hourPrecip":
+            hourlyPrecips.forEach((precip, index) => {
+                tableData[index].textContent = precip + "%";
+            })
+            break;
+        case "hourWind":
+            hourlyWind.forEach((wind, index) => {
+                tableData[index].textContent = wind;
+            })
+            break;
+    }
 }
 
 searchbar.addEventListener('keydown', (event) => {
@@ -33,15 +96,52 @@ searchbar.addEventListener('keydown', (event) => {
     }
 });
 
+toggleSwitch.addEventListener('input', () =>{
+    if(toggleSwitch.checked){
+        toCelcius = true;
+    }
+    else{
+        toCelcius = false;
+    }
+    getWeather(place)
+});
+
+
+let hourlyTemps = [];
+let hourlyPrecips = [];
+let hourlyWind = [];
+let times;
+
+function updateHourly(data){
+    const date = new Date();
+    times = [date.getHours(), date.getHours() + 2, date.getHours() + 4, date.getHours() + 6, date.getHours() + 8];
+    times = times.map((hour) => {
+        if(hour > 24){
+            return hour - 24;
+        } else {
+            return hour;
+        }
+    });
+
+    times.forEach((hour, index) => {
+        hourlyTemps[index] = data.hours[hour - 1].temp;
+        hourlyPrecips[index] = data.hours[hour - 1].precipprob;
+        hourlyWind[index] = data.hours[hour - 1].windspeed;
+    })
+}
+
 
 if ("geolocation" in navigator) {
     navigator.geolocation.getCurrentPosition((position) => {
-        console.log(position);
         reverseGeocode(position.coords.latitude, position.coords.longitude);
-    }, (err) => console.log(err));
+    }, (err) => {console.log(err)
+        place = "New York";
+        getWeather(place);
+    });
 } else {
     console.log("Geolocation is not supported by this browser.");
 }
+
 
 async function reverseGeocode(lat, lng){
     try {
@@ -55,6 +155,22 @@ async function reverseGeocode(lat, lng){
         console.log(err);
     }
 }
+
+document.querySelector("#hourTemp").addEventListener('click', () => {
+    hourlyData = "hourTemp";
+    hourlyDisplay(hourlyData);
+});
+
+document.querySelector("#hourPrecip").addEventListener('click', () => {
+    hourlyData = "hourPrecip";
+    hourlyDisplay(hourlyData);
+});
+
+document.querySelector("#hourWind").addEventListener('click', () => {
+    hourlyData = "hourWind";
+    hourlyDisplay(hourlyData);
+});
+
 
 
 
